78b7a733670d8f8885d9c7c62ed5e23c1ed2596f
urushi <krasavaagadaok@gmail.com>
2025-03-03 14:12:23 +0300

    refactored positioning of ui objects for easier scoping

--- View/UI.py
+++ View/UI.py
@@ -10,13 +10,29 @@ class UiManager:
     """
 
     def __init__(self, root, QOL, infoVariables, scale_factor=1):
+        # инициализация объектов переменных для объектов интерфейса
+        self.label_success_info = None
+        self.label_error_info = None
+        self.label_footer_info = None
+        self.label_daily_info = None
+        self.t_set_up_shifts_for_all_days_check = None
+        self.t_income_from_shops_check = None
+        self.label_income_info = None
+        self.delete_button = None
+        self.label_wages_info = None
+        self.t_wages_whole_month_check = None
+        self.labelDeleteInfo = None
+        self.toggle_button = None
+        self.label_instructions = None
+        self.label_period_info = None
+
         self.root = root
         self.QOL = QOL
         self.infoVariables = infoVariables
         self.scale_factor = scale_factor
         self.days_in_month = 31  # значение периода по умолчанию
 
-        # Tkinter переменные для чекбоксов
+        # переменные для чекбоксов
         self.t_wages_whole_month_var1 = BooleanVar()
         self.t_income_from_shops_var2 = BooleanVar()
         self.t_set_up_shifts_for_all_days_var3 = BooleanVar()
@@ -24,6 +40,7 @@ class UiManager:
         self.presenter = None  # Presenter будет установлен извне
 
         self.setup_ui()
+
 
     # Методы, вызываемые Presenter-ом для отображения сообщений:
     def nothing_picked(self):
@@ -54,8 +71,8 @@ class UiManager:
             print(self.presenter)
 
     def toggle_days(self):
-        # Логика переключения периода – можно оставить в View (так как это связано с отображением)
-        if (self.t_wages_whole_month_var1.get() or self.t_set_up_shifts_for_all_days_var3.get()):
+        # Логика переключения периода – можно оставить в View
+        if self.t_wages_whole_month_var1.get() or self.t_set_up_shifts_for_all_days_var3.get():
             self.label_error_info.config(text=" ")
         if self.days_in_month == 15:
             self.days_in_month = 31
@@ -83,14 +100,13 @@ class UiManager:
         padx = int(10 * self.scale_factor)
         pady = int(10 * self.scale_factor)
 
-        months = [
-            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
-            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
-        ]
+
         num_columns = 4  # число колонок
 
+        #BUTTONS
+
         # Создаем кнопки для каждого месяца
-        for i, month in enumerate(months):
+        for i, month in enumerate(self.infoVariables.months):
             button = tk.Button(self.root, text=month, height=2, width=btn_width,
                                command=lambda m=month: self.on_button_click(m),
                                bg="white", fg="black",
@@ -103,27 +119,8 @@ class UiManager:
         # Равномерное распределение колонок и строк
         for i in range(num_columns):
             self.root.grid_columnconfigure(i, weight=1, uniform="equal")
-        for i in range((len(months) + num_columns - 1) // num_columns):
+        for i in range((len(self.infoVariables.months) + num_columns - 1) // num_columns):
             self.root.grid_rowconfigure(i, weight=1, uniform="equal")
-
-        # Надпись инфа про период
-        self.label_period_info = tk.Label(
-            self.root,
-            text=f"Count from {self.days_in_month - 15}    to:    {self.days_in_month}",
-            font=("Roboto", int(25 * self.scale_factor), "bold"),
-            bg="black", fg="white"
-        )
-        self.label_period_info.grid(
-            row=6, column=0, pady=pady, padx=padx, columnspan=3)
-
-        # Надписи инструкции
-        self.label_instructions = tk.Label(
-            self.root,
-            text=self.infoVariables.infoAboutPeriodsAndbuttons,
-            font=("Roboto", int(15 * self.scale_factor), "bold"),
-            bg="black", fg="white"
-        )
-        self.label_instructions.grid(row=3, column=1, pady=pady, columnspan=2)
 
         # Переключить РП
         self.toggle_button = tk.Button(
@@ -147,11 +144,69 @@ class UiManager:
         self.delete_button.grid(
             row=7, column=3, pady=int(30 * self.scale_factor))
 
+
+        #CHECK BOXES
+
+        # Переключатель смены за весь РП
+        self.t_wages_whole_month_check = tk.Checkbutton(
+            self.root,
+            text="All shifts for period?",
+            variable=self.t_wages_whole_month_var1,
+            bg="white", fg="black",
+            font=("Roboto", int(24 * self.scale_factor), "bold")
+        )
+        self.t_wages_whole_month_check.grid(
+            row=7, column=0, pady=pady, padx=padx)
+
+        # Переключатель прихода
+        self.t_income_from_shops_check = tk.Checkbutton(
+            self.root,
+            text=self.infoVariables.income_from_shopsINFO,
+            variable=self.t_income_from_shops_var2,
+            bg="white", fg="black",
+            font=("Roboto", int(24 * self.scale_factor), "bold")
+        )
+        self.t_income_from_shops_check.grid(
+            row=7, column=2, pady=pady, padx=padx)
+
+        # Переключатель смены на все дни
+        self.t_set_up_shifts_for_all_days_check = tk.Checkbutton(
+            self.root,
+            text="Shifts for every day?",
+            variable=self.t_set_up_shifts_for_all_days_var3,
+            bg="white", fg="black",
+            font=("Roboto", int(24 * self.scale_factor), "bold")
+        )
+        self.t_set_up_shifts_for_all_days_check.grid(
+            row=7, column=1, pady=pady, padx=padx)
+
+
+        #LABES
+
+        # Надпись инфа про период
+        self.label_period_info = tk.Label(
+            self.root,
+            text=f"Count from {self.days_in_month - 15}    to:    {self.days_in_month}",
+            font=("Roboto", int(25 * self.scale_factor), "bold"),
+            bg="black", fg="white"
+        )
+        self.label_period_info.grid(
+            row=6, column=0, pady=pady, padx=padx, columnspan=3)
+
+        # Надписи инструкции
+        self.label_instructions = tk.Label(
+            self.root,
+            text=self.infoVariables.infoAboutPeriodsAndbuttons,
+            font=("Roboto", int(15 * self.scale_factor), "bold"),
+            bg="black", fg="white"
+        )
+        self.label_instructions.grid(row=3, column=1, pady=pady, columnspan=2)
+
         # Надпись инфа про удаление всех данных из ТБ
         self.labelDeleteInfo = tk.Label(
             self.root,
             text=self.infoVariables.infoAboutDeleteButton,
-            font=("Roboto", int(15 * self.scale_factor), "bold"),
+            font=("Roboto", int(19 * self.scale_factor), "bold"),
             bg="black", fg="white"
         )
         self.labelDeleteInfo.grid(row=8, column=3)
@@ -166,17 +221,6 @@ class UiManager:
         self.label_wages_info.grid(
             row=8, column=1, pady=int(20 * self.scale_factor))
 
-        # Переключатель смены за весь РП
-        self.t_wages_whole_month_check = tk.Checkbutton(
-            self.root,
-            text="All shifts for period?",
-            variable=self.t_wages_whole_month_var1,
-            bg="white", fg="black",
-            font=("Roboto", int(24 * self.scale_factor), "bold")
-        )
-        self.t_wages_whole_month_check.grid(
-            row=7, column=0, pady=pady, padx=padx)
-
         # Надпись инфа про приходную функцию
         self.label_income_info = tk.Label(
             self.root,
@@ -187,16 +231,14 @@ class UiManager:
         self.label_income_info.grid(
             row=8, column=0, pady=int(20 * self.scale_factor))
 
-        # Переключатель прихода
-        self.t_income_from_shops_check = tk.Checkbutton(
+        # Надпись внизу
+        self.label_footer_info = tk.Label(
             self.root,
-            text="Income from arenas?",
-            variable=self.t_income_from_shops_var2,
-            bg="white", fg="black",
-            font=("Roboto", int(24 * self.scale_factor), "bold")
+            text="why get less?\nwhen u can get more\nver 0.1.4",
+            bg="black", fg="white",
+            font=("Arial", int(14 * self.scale_factor), "bold")
         )
-        self.t_income_from_shops_check.grid(
-            row=7, column=2, pady=pady, padx=padx)
+        self.label_footer_info.grid(row=10, column=0, columnspan=4)
 
         # Надпись инфа про каждодневную функцию
         self.label_daily_info = tk.Label(
@@ -207,26 +249,6 @@ class UiManager:
         )
         self.label_daily_info.grid(
             row=8, column=2, pady=int(18 * self.scale_factor))
-
-        # Переключатель смены на все дни
-        self.t_set_up_shifts_for_all_days_check = tk.Checkbutton(
-            self.root,
-            text="Shifts for every day?",
-            variable=self.t_set_up_shifts_for_all_days_var3,
-            bg="white", fg="black",
-            font=("Roboto", int(24 * self.scale_factor), "bold")
-        )
-        self.t_set_up_shifts_for_all_days_check.grid(
-            row=7, column=1, pady=pady, padx=padx)
-
-        # Надпись внизу
-        self.label_footer_info = tk.Label(
-            self.root,
-            text="why get less?\nwhen u can get more\nver 0.1.4",
-            bg="black", fg="white",
-            font=("Arial", int(14 * self.scale_factor), "bold")
-        )
-        self.label_footer_info.grid(row=10, column=0, columnspan=4)
 
         # Надпись ошибка
         self.label_error_info = tk.Label(
